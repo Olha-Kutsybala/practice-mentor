5f01c884626f6267cc012e09011faf988e4a054f
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-04-17 15:53:58 +0300

    1

---
+++ src/js/login-form.js
@@ -0,0 +1,56 @@
+// Якщо імейл і пароль користувача збігаються, зберігайте дані з форми при сабмите
+// у локальне сховище і змінюй кнопку login на logout і роби поля введення
+// Недоступними зміни.
+
+// При перезавантаженні сторінки, якщо користувач залогінений, ми маємо бачити logout-кнопку
+// та недоступні для зміни поля з даними користувача.
+// Клік по кнопці logout повертає все до початкового вигляду і видаляє дані користувача
+// З локального сховища.
+
+// Якщо введені дані не збігаються з потрібними даними, викликати аlert і
+// повідомляти про помилку.
+
+import { Notify } from 'notiflix';
+import { refs } from './refs';
+
+const USER_DATA = {
+  email: 'user@mail.com',
+  password: '123',
+};
+
+refs.loginForm.addEventListener('submit', onLoginFormSubmit);
+
+function onLoginFormSubmit(evt) {
+  evt.preventDefault();
+  const { email, password, button } = evt.target;
+  if (
+    email.value !== USER_DATA.email ||
+    password.value !== USER_DATA.password
+  ) {
+    Notify.failure('Невірний пароль або email');
+    return;
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+  button.textContent = 'logout';
+  email.disabled = true;
+  password.disabled = true;
+}
+
+function populateForm() {
+  const saveData = JSON.parse(localStorage.getItem('formData'));
+  if (!saveData) {
+    return;
+  }
+  refs.emailInput.disabled = true;
+  refs.passwordInput.disabled = true;
+  refs.emailInput.value = saveData.email;
+  refs.passwordInput.value = saveData.password;
+  refs.buttonSubmit.textContent = 'logout';
+}
+
+populateForm();
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+  loginForm: document.querySelector('#login-form'),
+  emailInput: document.querySelector('[type="email"]'),
+  passwordInput: document.querySelector('[type="password"]'),
+  buttonSubmit: document.querySelector('[type="submit"]'),
+};
